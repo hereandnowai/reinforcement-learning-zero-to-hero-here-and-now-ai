d96dbd31ef91dceff62590114946a6c58a2c418b
ruthranraghavan <ruthran@hereandnow.co.in>
2025-08-02 07:53:07 +0530

    feat: scaffold project30_mu_zero_stochastic_adapt

---
+++ project30_mu_zero_stochastic_adapt/README.md
@@ -0,0 +1,9 @@
+# Project 30: Mu Zero Stochastic Adapt
+
+*   **Difficulty:** Advanced
+*   **Learning Objectives:**
+    *   TODO: Add learning objectives
+*   **Algorithm(s):** MuZero
+*   **Environment:** Custom Stochastic Environment
+*   **Quick start:** `python src/main.py`
+*   **Estimated Effort:** 25-40 hours
---
+++ project30_mu_zero_stochastic_adapt/src/environment.py
@@ -0,0 +1,18 @@
+import gym
+
+class Env:
+    def __init__(self):
+        # TODO: Initialize your environment here
+        pass
+
+    def reset(self):
+        # TODO: Reset the environment and return the initial state
+        pass
+
+    def step(self, action):
+        # TODO: Take an action, return next_state, reward, done, info
+        pass
+
+    def render(self):
+        # TODO: Render the environment
+        pass
